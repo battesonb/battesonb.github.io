ebc5224024cc1cc8689010b3b8421358da966027
Byron Batteson <byronbatteson@gmail.com>
2023-06-21 09:47:16 +0200

    Fix deployment directory for pgfplots plugin

--- docs/_plugins/jekyll-pgfplots.rb
+++ docs/_plugins/jekyll-pgfplots.rb
@@ -20,7 +20,7 @@ module Jekyll
         if is_draft
           tmp_dir
         else
-          File.join(Dir.pwd, 'pgf', page_path)
+          File.join(Dir.pwd, 'assets/pgf', page_path)
         end
       dest_path = File.join(dest_dir, "#{@file_name}.svg")
       FileUtils.mkdir_p(dest_dir)
@@ -37,7 +37,7 @@ module Jekyll
         if is_draft
           File.join('/pgf_tmp', page_path, "#{@file_name}.svg")
         else
-          File.join('/pgf', page_path, "#{@file_name}.svg")
+          File.join('/assets/pgf', page_path, "#{@file_name}.svg")
         end
       "<embed src=\"#{web_dest_path}\" type=\"image/svg+xml\" />"
     end
